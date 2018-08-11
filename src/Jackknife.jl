d02944ea720093f51f8b25493b336dd5ae0aef4b
Alex Arslan <ararslan@comcast.net>
2018-08-11 12:42:52 +0100

    Various changes
    * Drop 0.6
    * Improve docs
    * Introduce a type that enforces reducing behavior
    * Don't use generators--gross
    * Use the Statistics stdlib package

--- src/Jackknife.jl
+++ src/Jackknife.jl
@@ -1,18 +1,36 @@
-__precompile__()
-
 module Jackknife
 
-"""
-    Jackknife.leaveoneout(estimator::Function, x)
+using Statistics
 
-Estimate the parameter `estimator(x)` for each subsample of `x`, systematically
-omitting each index one at a time. The result is a vector of length `length(x)-1`
-of parameter estimates.
 """
-function leaveoneout(estimator::Function, x::AbstractVector{<:Real})
+    Reducer(f)
+
+Type that holds a function and enforces at runtime that the function returns either a
+`Real` value or `missing`. This is used internally to sanity check the input for functions
+which accept a function argument.
+"""
+struct Reducer
+    f::Function
+end
+(r::Reducer)(x) = r.f(x)::Union{Real,Missing}
+
+const FuncOrReducer = Union{Function,Reducer}
+
+
+"""
+    Jackknife.leaveoneout(estimator::Function, x::AbstractVector{<:Real})
+
+Estimate the parameter ``\\hat{\\theta}`` using the estimating function `estimator`
+for each subsample of `x`, systematically omitting each index one at a time. The
+result is a vector of length `length(x)-1` of parameter estimates.
+"""
+leaveoneout(estimator::Function, x::AbstractVector{T}) where {T<:Union{Real,Missing}} =
+    leaveoneout(Reducer(estimator), x)
+
+function leaveoneout(estimator::Reducer, x::AbstractVector{T}) where T<:Union{Real,Missing}
     length(x) > 1 || throw(ArgumentError("The sample must have size > 1"))
     inds = eachindex(x)
-    return [estimator(x[filter(j -> j != i, inds)]) for i in inds]
+    return map(i->estimator(view(x, filter(!isequal(i), inds))), inds)
 end
 
 
@@ -22,10 +40,10 @@ end
 Compute the jackknife estimate of the variance of `estimator`, which is given as a
 function that computes a point estimate when passed a real-valued vector `x`.
 """
-function variance(estimator::Function, x::AbstractVector{<:Real})
+function variance(estimator::FuncOrReducer, x::AbstractVector{T}) where T<:Union{Real,Missing}
     θ = leaveoneout(estimator, x)
     n = length(x)
-    return Base.var(θ) * (n - 1)^2 / n
+    return var(θ) * (n - 1)^2 / n
 end
 
 
@@ -35,7 +53,7 @@ end
 Compute the jackknife estimate of the bias of `estimator`, which is given as a
 function that computes a point estimate when passed a real-valued vector `x`.
 """
-function bias(estimator::Function, x::AbstractVector{<:Real})
+function bias(estimator::FuncOrReducer, x::AbstractVector{T}) where {T<:Union{Real,Missing}}
     θ = leaveoneout(estimator, x)
     return (length(x) - 1) * (mean(θ) - estimator(x))
 end
@@ -46,7 +64,7 @@ end
 
 Compute the bias-corrected jackknife estimate of the parameter `estimator(x)`.
 """
-function estimate(estimator::Function, x::AbstractVector{<:Real})
+function estimate(estimator::FuncOrReducer, x::AbstractVector{T}) where T<:Union{Real,Missing}
     θ = leaveoneout(estimator, x)
     n = length(x)
     return n * estimator(x) - (n - 1) * mean(θ)
